831d7393998ff26cd182c847f9cf3ac915b4560b
Rafael Scremim <rafaelscremim1997@gmail.com>
2022-09-11 12:06:48 -0300

    Criado calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; // Referencia para o namespace chamar o system, os comandos estão dentro do namespace.
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora //Agrupamento de classes relacionadas.
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
